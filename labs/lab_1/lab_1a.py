13388d4ee29e5be4075d9fe6c883e2da24ebd9f8
turtle-ed <100389504+turtle-ed@users.noreply.github.com>
2026-03-23 21:07:38 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,7 +4,7 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
-robot_speed = 10
+robot_speed = 20
 
 def main():
     print("Hello World!")
