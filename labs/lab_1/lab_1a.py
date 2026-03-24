8e98c0a24e5dffb4803f30b8f001a8bfcf4ed454
turtle-ed <100389504+turtle-ed@users.noreply.github.com>
2026-03-23 21:01:14 -0400

    robot speed variable

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,6 +4,7 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
+robot_speed = 10
 
 def main():
     print("Hello World!")
